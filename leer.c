9d8c985afb08effd97c1b02cc98811621a1d356b
Andrés Muñoz <andres.munoz.b@usach.cl>
2020-10-26 22:59:48 -0300

    CHG: Se modifica lectura de archivo
    Signed-off-by: Andrés Muñoz <andres.munoz.b@usach.cl>

--- leer.c
+++ leer.c
@@ -1,26 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * read_file: Lectura de un archivo
+ * Entrada:
+ *      - file_name: Nombre de entrada del archivo
+ *      - size: Cantidad de elementos dentro del archivo
+ * Salida: arreglo de float con valores del archivo.
+*/
+void read_file(char *file_name, float* values, int size)
+{
+    FILE *input_file = fopen(file_name, "rb"); // Abrir archivo
+    
+    float buffer; // Buffer de lectura
+    for (int i = 0; i < size; i++)
+    {
+        fread(&buffer, sizeof(float), 1, input_file); // Leer un valor desde el archivo
+        values[i] = buffer; // Agregar el valor en el arreglo
+    }
+    fclose(input_file); //Cerrar archivo
+}
+/**
+ * print_fa: 
+ * Entrada:
+ *      - values: arreglo de floats 
+ *      - size: largo del arreglo
+*/
+void print_values(float* values, int size){
+    for (int i=0;i<size;i++){
+        printf("%f\n", values[i]);
+    }
+}
 int main()
 {
-
     char *input_file_name = "data/64floats.raw";
     char *output_file_name = "output.raw";
-    int N = 64;
+    int size = 64;
     int d = 0;
-    float *values = (float *)malloc(sizeof(float) * N);
-    float value;
-    int i = 0;
-
-    FILE *input_file = fopen(input_file_name,"rb");
-    while (i != N)
-    {
-        fread(&value, sizeof(float), 1, input_file);
-        values[i] = value;
-        printf("%f\n",value);
-        i++;
-    }
-    printf("%s\n", input_file_name);
+    float *values = (float *)malloc(sizeof(float) * size); // Reservar memoria para arreglo de valores
+    read_file(input_file_name, values, size);
+    print_values(values,size);
     free(values);
     return 1;
 }
