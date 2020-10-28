d13e76c4e868cfa653328b846d57bb24eaf6f9d5
Andrés Muñoz <andres.munoz.b@usach.cl>
2020-10-28 20:05:47 -0300

    CHG: Se parametriza bmn
    Signed-off-by: Andrés Muñoz <andres.munoz.b@usach.cl>

--- bmn.c
+++ bmn.c
@@ -20,6 +20,42 @@ void print_matrix_16(float *arg1, float *arg2, float *arg3, float *arg4)
     printf("\n");
 }
 
+void bmn_network(__m128 *r1, __m128 *r2){
+    __m128 t1,t2;
+
+    // PRIMER PASO
+    *r1 = _mm_shuffle_ps(*r1, *r1, _MM_SHUFFLE(3,1,2,0)); // INVERTIR AL MEDIO
+    *r2 = _mm_shuffle_ps(*r2, *r2, _MM_SHUFFLE(0,2,1,3)); // INVERTIR LOS EXTREMOS
+
+    t1 = _mm_max_ps(*r1, *r2); 
+    t2 = _mm_min_ps(*r1, *r2);
+
+    // SEGUNDO PASO
+    *r1 = _mm_shuffle_ps(t1, t2, _MM_SHUFFLE(2,0,2,0)); 
+    *r2 = _mm_shuffle_ps(t1, t2, _MM_SHUFFLE(3,1,3,1));
+    *r1 = _mm_shuffle_ps(*r1, *r1, _MM_SHUFFLE(3,1,2,0)); // INVERTIR AL MEDIO
+    *r2 = _mm_shuffle_ps(*r2, *r2, _MM_SHUFFLE(3,1,2,0)); // INVERTIR AL MEDIO
+
+    t1 = _mm_max_ps(*r1, *r2);
+    t2 = _mm_min_ps(*r1, *r2);
+    
+    // TERCER PASO
+    *r1 = _mm_shuffle_ps(t1, t2, _MM_SHUFFLE(1,0,1,0)); 
+    *r2 = _mm_shuffle_ps(t1, t2, _MM_SHUFFLE(3,2,3,2));
+    *r1 = _mm_shuffle_ps(*r1, *r1, _MM_SHUFFLE(3,1,2,0)); // INVERTIR AL MEDIO
+    *r2 = _mm_shuffle_ps(*r2, *r2, _MM_SHUFFLE(3,1,2,0)); // INVERTIR AL MEDIO
+
+    t1 = _mm_max_ps(*r1, *r2);
+    t2 = _mm_min_ps(*r1, *r2);
+
+    // FINAL
+    *r1 = _mm_shuffle_ps(t1, t2, _MM_SHUFFLE(1,0,1,0)); 
+    *r2 = _mm_shuffle_ps(t1, t2, _MM_SHUFFLE(3,2,3,2));
+    *r1 = _mm_shuffle_ps(*r1, *r1, _MM_SHUFFLE(3,1,2,0)); // INVERTIR AL MEDIO
+    *r2 = _mm_shuffle_ps(*r2, *r2, _MM_SHUFFLE(3,1,2,0)); // INVERTIR AL MEDIO
+}
+
+
 int main()
 {
     
@@ -44,44 +80,12 @@ int main()
     r1 = _mm_load_ps(A1);
     r2 = _mm_load_ps(A2);
 
-    
-    /* PRIMER PASO */
-    r1 = _mm_shuffle_ps(r1, r1, _MM_SHUFFLE(3,1,2,0)); // INVERTIR AL MEDIO
-    r2 = _mm_shuffle_ps(r2, r2, _MM_SHUFFLE(0,2,1,3)); // INVERTIR LOS EXTREMOS
-
-    t1 = _mm_max_ps(r1, r2); 
-    t2 = _mm_min_ps(r1, r2);
-
-    /* SEGUNDO PASO*/
-    r1 = _mm_shuffle_ps(t1, t2, _MM_SHUFFLE(2,0,2,0)); 
-    r2 = _mm_shuffle_ps(t1, t2, _MM_SHUFFLE(3,1,3,1));
-    r1 = _mm_shuffle_ps(r1, r1, _MM_SHUFFLE(3,1,2,0)); // INVERTIR AL MEDIO
-    r2 = _mm_shuffle_ps(r2, r2, _MM_SHUFFLE(3,1,2,0)); // INVERTIR AL MEDIO
-
-    t1 = _mm_max_ps(r1, r2);
-    t2 = _mm_min_ps(r1, r2);
-    
-    /* TERCER PASO*/
-    r1 = _mm_shuffle_ps(t1, t2, _MM_SHUFFLE(1,0,1,0)); 
-    r2 = _mm_shuffle_ps(t1, t2, _MM_SHUFFLE(3,2,3,2));
-    r1 = _mm_shuffle_ps(r1, r1, _MM_SHUFFLE(3,1,2,0)); // INVERTIR AL MEDIO
-    r2 = _mm_shuffle_ps(r2, r2, _MM_SHUFFLE(3,1,2,0)); // INVERTIR AL MEDIO
-
-    t1 = _mm_max_ps(r1, r2);
-    t2 = _mm_min_ps(r1, r2);
-
-    /* FINAL */
-    r1 = _mm_shuffle_ps(t1, t2, _MM_SHUFFLE(1,0,1,0)); 
-    r2 = _mm_shuffle_ps(t1, t2, _MM_SHUFFLE(3,2,3,2));
-    r1 = _mm_shuffle_ps(r1, r1, _MM_SHUFFLE(3,1,2,0)); // INVERTIR AL MEDIO
-    r2 = _mm_shuffle_ps(r2, r2, _MM_SHUFFLE(3,1,2,0)); // INVERTIR AL MEDIO
+    // BMN NETWORK
+    bmn_network(&r1,&r2);
 
     //GUARDAR REGISTROS
     _mm_store_ps(A1, r1);
     _mm_store_ps(A2, r2);
-
-    
-    
 
     print_float_array(A1,4);
     printf("\n");
